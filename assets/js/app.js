39f2a1c8d7ef076ffdef481517f25afdbd2f2273
Danilo Gonzalez <gonzalezantonio.danilo@gmail.com>
2017-08-20 19:54:05 -0700

    Created a static landing page using particlesjs

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#fff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 1,
+        "color": "#000"
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#fff",
+      "opacity": 0.5,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "out_mode": "out",
+    }
+  }
+});
